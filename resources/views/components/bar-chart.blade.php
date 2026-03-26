d0b0c6abe9cbf6b35fc833b0e5cc4c5cad15d7e7
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-03-26 17:56:54 +0600

    create side nav, top nav, barchart component

---
+++ resources/views/components/bar-chart.blade.php
@@ -0,0 +1,13 @@
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-title">
+                                    <h4>Fee Collections and Expenses</h4>
+
+                                </div>
+                                <div class="card-body">
+                                    <div class="ct-bar-chart m-t-30"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
---
+++ resources/views/components/side-nav.blade.php
@@ -0,0 +1,26 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+<div class="nano">
+            <div class="nano-content">
+                <ul>
+                    <div class="logo"><a href="index.html">
+                            <!-- <img src="images/logo.png" alt="" /> --><span>Appointly</span></a></div>
+                    <li class="label">Main</li>
+                    <li><a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard </a>
+
+                    </li>
+
+                    <li class="label">Apps</li>
+                    <li><a href="app-event-calender.html"><i class="ti-calendar"></i> Calender </a></li>
+                    <li><a href="app-email.html"><i class="ti-email"></i> Appointments </a></li>
+                    <li><a href="app-email.html"><i class="ti-email"></i> Customers </a></li>
+                    <li><a href="app-email.html"><i class="ti-email"></i> Services </a></li>
+                    <li><a href="app-email.html"><i class="ti-user"></i> Users </a></li>
+                    <li><a href="app-email.html"><i class="ti-email"></i> Notifications </a></li>
+                    <li><a href="app-profile.html"><i class="ti-user"></i> Profile</a></li>
+
+                    <li><a><i class="ti-close"></i> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+</div>
+
